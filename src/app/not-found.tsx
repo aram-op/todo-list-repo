dccb6b9931df51db94ab71fac61590f3ecc043dc
aram-op <aram.tadevosyan@optimumpartners.co>
2024-12-10 12:01:27 +0400

    Implemented not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import './globals.css';
+
+function  NotFound() {
+    return(
+        <div className="errorContainer">
+            <h2>404 Not Found</h2>
+        </div>
+    );
+}
+
+export default NotFound;
--- src/app/todos/[id]/edit/page.tsx
+++ src/app/todos/[id]/edit/page.tsx
@@ -1,10 +1,13 @@
 import EditForm from '@/app/ui/edit form/edit-form';
 import {fetchTodoById} from '@/app/lib/data';
 import styles from '@/app/page.module.css';
+import {notFound} from 'next/navigation';
 
 export default async function Page({params}: { params: Promise<{ id: string }> }) {
     const id = (await params).id;
     const todo = await fetchTodoById(id);
+
+    if(!todo) notFound();
 
     return (
         <div className={styles.container}>
