b858fc92f46dbfa0dc2736cef747ea6fffdc5468
aram-op <aram.tadevosyan@optimumpartners.co>
2024-12-10 11:15:56 +0400

    Added error messages for input fields

--- src/app/ui/create-form/create-form.tsx
+++ src/app/ui/create-form/create-form.tsx
@@ -4,8 +4,11 @@ import {addTodo} from '@/app/lib/data';
 import {redirect} from 'next/navigation';
 import styles from './create-form.module.css';
 import {poppins} from '@/app/ui/fonts';
+import {useState} from 'react';
 
 function CreateForm() {
+    const [error, setError] = useState<{message: string} | null>(null);
+
     async function create(formData: FormData) {
         const rawFormData = {
             title: formData.get('title'),
@@ -15,14 +18,23 @@ function CreateForm() {
             await addTodo(rawFormData.title.toString());
             redirect('/');
         }
+        setError({message: 'Title field cannot be empty!'});
+    }
+
+    function handleInputChange(input: string) {
+        if(input.length > 0) {
+            setError(null);
+        }
     }
 
     return (
         <form className={styles.form} action={create}>
+            <p className={styles.errorMessage}>{error && error.message}</p>
             <input
                 className={`${styles.title} ${poppins.className}`}
                 name="title"
                 placeholder="... needs to be done"
+                onChange={(e) => handleInputChange(e.target.value)}
             />
 
             <button className={`${styles.create} ${poppins.className}`} type={'submit'}>Create</button>
--- src/app/ui/edit form/edit-form.tsx
+++ src/app/ui/edit form/edit-form.tsx
@@ -5,25 +5,39 @@ import {redirect} from 'next/navigation';
 import styles from './edit-form.module.css';
 import {poppins} from '@/app/ui/fonts';
 import {Todo} from '@/app/lib/definitions';
+import {useState} from 'react';
 
 function EditForm({todo}: { todo: Todo }) {
+    const [error, setError] = useState<{message: string} | null>(null);
+
     async function edit(formData: FormData) {
         const rawFormData = {
             title: formData.get('title'),
         };
 
-        if (rawFormData.title) {
+        if(rawFormData.title && rawFormData.title === todo.title) {
+            return;
+        } else if (rawFormData.title) {
             await updateTodo({...todo, title: rawFormData.title.toString()});
             redirect('/');
+        }
+        setError({message: 'Title field cannot be empty!'});
+    }
+
+    function handleInputChange(input: string) {
+        if(input.length > 0) {
+            setError(null);
         }
     }
 
     return (
         <form className={styles.form} action={edit}>
+            <p className={styles.errorMessage}>{error && error.message}</p>
             <input
                 className={`${styles.title} ${poppins.className}`}
                 name="title"
                 defaultValue={todo.title}
+                onChange={(e) => handleInputChange(e.target.value)}
             />
 
             <button className={`${styles.edit} ${poppins.className}`} type={'submit'}>Save Changes</button>
