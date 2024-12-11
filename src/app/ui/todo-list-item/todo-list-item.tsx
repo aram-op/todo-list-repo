2ca2cd0fbe1658b317beb3ca7757de8296906d0c
aram-op <aram.tadevosyan@optimumpartners.co>
2024-12-11 13:25:39 +0400

    Improved error handling, event handling in TodoListItem component

--- src/app/ui/todo-list-item/todo-list-item.tsx
+++ src/app/ui/todo-list-item/todo-list-item.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import {useState} from 'react';
+import {useCallback, useState} from 'react';
 import styles from './todo-list-item.module.css';
 import {Todo} from '@/app/lib/definitions';
 import {deleteTodoById, updateTodo} from '@/app/lib/data';
@@ -13,28 +13,37 @@ function TodoListItem({todo, onItemRemoved}: {
     const [isDone, setIsDone] = useState(todo.is_completed);
     const router = useRouter();
 
-    async function handleMarkDone() {
-        setIsDone(!isDone);
-        await updateTodo({...todo, is_completed: !isDone});
+    function handleMarkDone() {
+        try {
+            setIsDone(i => !i);
+            updateTodo({...todo, is_completed: !isDone});
+        } catch (e) {
+            setIsDone((val) => !val);
+            throw e;
+        }
     }
 
-    async function handleRemove() {
-        await deleteTodoById(todo.id);
+    function handleRemove() {
+        try {
+            deleteTodoById(todo.id);
+        } catch (e) {
+            throw e;
+        }
         onItemRemoved(todo.id);
     }
 
-    function handleSelectItem() {
+    const handleSelectItem = useCallback(() => {
         router.push(`todos/${todo.id}/edit`);
-    }
+    }, [todo]);
 
     return (
         <li className={styles.item}>
             <p onClick={handleSelectItem}
                className={`${styles.text} ${isDone ? styles.completed : ''}`}>{todo.title}</p>
-            <button onClick={() => handleMarkDone()} className={styles.complete}>
+            <button onClick={handleMarkDone} className={styles.complete}>
                 <img src="check-mark.svg" width="20" height="20" alt="complete"/>
             </button>
-            <button onClick={() => handleRemove()} className={styles.delete}>
+            <button onClick={handleRemove} className={styles.delete}>
                 <img src="remove.svg" width="25" height="25" alt="remove"/>
             </button>
         </li>
